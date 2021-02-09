a08640c124804ab04fb1fb8ce50d1a086a0ffb6a
Jack Lisov <53009936+jacklisov@users.noreply.github.com>
2021-02-09 14:10:53 +0300

    Update 1.py
    Либа: pypiwin32

--- 1.py
+++ 1.py
@@ -1,13 +1,34 @@
-from vosk import Model, KaldiRecognizer 
+from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import json
+import win32com.client as win32
+from pathlib import Path
+
+word = None
 
 if not os.path.exists("model-ru"):
     print("No models found!")
     exit(1)
 
+
+def call(a):
+    if a == "текст":
+        print("Result: ", a)
+        return 1
+    if a == "запустить программу":
+        word = win32.gencache.EnsureDispatch("Word.Application")
+        word.Visible = True
+        return 1
+    if a == "открыть файл":
+        f = os.path.abspath(os.curdir) + "/test.docx"
+        word = win32.gencache.EnsureDispatch("Word.Application")
+        word.Documents.Open(f)
+        return 1
+
 audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = audio.open(format=pyaudio.paInt16, channels=1,
+                    rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
 model = Model("model-ru")
@@ -15,12 +36,11 @@ recognizer = KaldiRecognizer(model, 16000)
 
 while True:
     data = stream.read(2000, exception_on_overflow=False)
-   
+
     if len(data) == 0:
        break
     if recognizer.AcceptWaveform(data):
-        print(recognizer.Result())
+        a = json.loads(recognizer.Result())['text']
+        call(a)
     else:
-        print(recognizer.PartialResult())
-
-print(recognizer.FinalResult())
+        print(json.loads(recognizer.PartialResult())['partial'])
